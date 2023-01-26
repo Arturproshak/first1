30a68ec87c44cd2cf7c50056ef1e08a0a511e0a0
Artur <arturpro20091@gmail.com>
2023-01-26 19:26:43 +0200

    Version 0.2

---
+++ first1.py
@@ -0,0 +1,39 @@
+class Tree:
+    high = 24
+    name = "birch"
+    age = 67
+    type = "coniferous"
+    nests = 3
+
+print(Tree.high)
+print(Tree.name)
+print(Tree.age)
+print(Tree.type)
+print(Tree.nests)
+
+class Book:
+    page = 574
+    age = 37
+    name = "The Children of Captain Grant"
+    author = "Jules Verne"
+    chapter = 60
+
+print(Book.page)
+print(Book.age)
+print(Book.name)
+print(Book.author)
+print(Book.chapter)
+
+
+class Horse:
+    speed = 88
+    age = 15
+    name = "Storm"
+    heigt = 1,7
+    length = 2,4
+
+print(Horse.speed)
+print(Horse.age)
+print(Horse.name)
+print(Horse.heigt)
+print(Horse.length)
